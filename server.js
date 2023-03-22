172c8d3989bba8f703b325eb7f87053302253205
cecilia-89 <catabong89@gmail.com>
2023-03-22 15:15:47 +0100

    refactored to suite eslint

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const routes = require('./routes/index');
+import router from './routes/index';
 
 const app = express();
-app.use(express.json({ limit: '50mb' }));
+const port = process.env.PORT || 5000;
+app.use(express.json());
 
-const port = process.env.PORT ? process.env.PORT : 5000;
+app.use('/', router)
 
-routes(app);
 
 app.listen(port, () => {
   console.log('Server running on port 5000');
